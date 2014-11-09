7924e331a2f2bda58428e66caafebb81485a6edf
Suipotryot <you@example.com>
2014-11-09 23:34:02 +0100

    DEV - Tried to correct Encrypter but does not work

--- Encrypter.py
+++ Encrypter.py
@@ -8,21 +8,27 @@ class Encrypter:
 
     @staticmethod
     def decode(data):
-        return base64.b64decode(Encrypter.cesar_all(data, -1))
+        return Encrypter.cesar_all(base64.b64decode(data), -1)
 
     @staticmethod
     def cesar_all(string, decalage):
+        string = str(string)
+        print(string)
         res = ''
         for char in string:
             res += Encrypter.cesar(char, decalage)
-        return res
+        return bytes(res, 'UTF-8')
 
     @staticmethod
     def cesar(char, decalage):
         min_list = 'abcdefghijklmnopqrstuvwxyz'
         maj_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         if char in min_list:
-            return min_list[(min_list.index(char)+decalage)%26]
+            res = min_list[(min_list.index(char)+decalage)%26]
+            return res
         if char in maj_list:
             return maj_list[(maj_list.index(char)+decalage)%26]
         return char
+
+#print(Encrypter.encode('a'))
+print(Encrypter.decode(Encrypter.encode('azerty')))
